1f9da08641b76fab0903ed9e1c72c581df5d965b
Luiz Carlos <luiz_cjr@hotmail.com>
2022-10-29 12:42:38 -0300

    creating proguard rules creating key adding serialized name

--- app/src/main/java/com/example/movies/data/network/response/DataContainerResponse.kt
+++ app/src/main/java/com/example/movies/data/network/response/DataContainerResponse.kt
@@ -3,10 +3,12 @@ package com.example.movies.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class DataContainerResponse<T>(
+    @SerializedName("page")
     val page: Int,
     @SerializedName("total_results")
     val totalResults: Int,
     @SerializedName("total_pages")
     val totalPages: Int,
+    @SerializedName("results")
     val results: List<T>
 )
--- app/src/main/java/com/example/movies/data/network/response/MoviesResponse.kt
+++ app/src/main/java/com/example/movies/data/network/response/MoviesResponse.kt
@@ -3,14 +3,17 @@ package com.example.movies.data.network.response
 import com.google.gson.annotations.SerializedName
 
 data class MoviesResponse(
+    @SerializedName("id")
     val id: Int,
     @SerializedName("poster_path")
     val posterPath: String?,
+    @SerializedName("overview")
     val overview: String,
     @SerializedName("genre_ids")
     val genreIds: List<Int>,
     @SerializedName("original_title")
     val originalTitle: String,
+    @SerializedName("title")
     val title: String,
     @SerializedName("vote_average")
     val voteAverage: Float
